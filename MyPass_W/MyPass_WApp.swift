7d3949e4d21f51c657a8cd17e70da7aab9329b75
raphpay <41484808+raphpay@users.noreply.github.com>
2022-05-17 19:36:27 +0200

    Initial Commit

---
+++ MyPass_W/MyPass_WApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPass_WApp.swift
+//  MyPass_W
+//
+//  Created by Raphaël Payet on 17/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyPass_WApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
