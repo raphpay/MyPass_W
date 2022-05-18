2fbd76bba1d9a9eb4c94672488087e07b4cfc4c0
raphpay <41484808+raphpay@users.noreply.github.com>
2022-05-18 14:01:54 +0200

    Feature: Integrate a view model #3

--- MyPass_W/View/ContentView.swift
+++ MyPass_W/View/ContentView.swift
@@ -12,78 +12,39 @@ enum ValueType {
 }
 
 struct ContentView: View {
-    // TODO: Create a ViewModel
-    @State private var generatedPassword: String = ""
-    @State private var charactersValue: Double = 3
-    @State private var charactersRange: ClosedRange<Double> = 1...40
-    @State private var separatorValue: Double = 6
-    @State private var separatorRange: ClosedRange<Double> = 1...10
     
-    func decreaseValue(_ valueType: ValueType) {
-        switch valueType {
-        case .characters:
-            if (charactersValue > 1) {
-                charactersValue -= 1
-            }
-        case .separators:
-            if (separatorValue > 1) {
-                separatorValue -= 1
-            }
-        }
-    }
+    @StateObject var viewModel = ContentViewViewModel()
     
-    func increaseValue(_ valueType: ValueType) {
-        switch valueType {
-        case .characters:
-            if (charactersValue < 40) {
-                charactersValue += 1
-            }
-        case .separators:
-            if (separatorValue < 10) {
-                separatorValue += 1
-            }
-        }
-    }
-    
-    func generatePassword() {
-        print("generatePassword")
-    }
-    
-    func copyPassword() {
-        print("copyPassword")
-    }
-    
-    // TODO: Create components
     var body: some View {
         VStack {
             AppLogo()
             
-            PasswordTextField(generatedPassword: $generatedPassword)
+            PasswordTextField(generatedPassword: $viewModel.generatedPassword)
             
-            RangeSlider(value: $charactersValue, range: charactersRange) {
-                decreaseValue(.characters)
+            RangeSlider(value: $viewModel.charactersValue, range: viewModel.charactersRange) {
+                viewModel.decreaseValue(.characters)
             } onIncrease: {
-                increaseValue(.characters)
+                viewModel.increaseValue(.characters)
             }
             
             Spacer()
                 .frame(height: 100)
             
-            RangeSlider(value: $separatorValue, range: separatorRange) {
-                decreaseValue(.separators)
+            RangeSlider(value: $viewModel.separatorValue, range: viewModel.separatorRange) {
+                viewModel.decreaseValue(.separators)
             } onIncrease: {
-                increaseValue(.separators)
+                viewModel.increaseValue(.separators)
             }
 
             Spacer()
                 .frame(height: 100)
             
             RoundedButton(title: "Generate") {
-                generatePassword()
+                viewModel.generatePassword()
             }
             
             RoundedButton(title: "Copy") {
-                copyPassword()
+                viewModel.copyPassword()
             }
             
             Spacer()
