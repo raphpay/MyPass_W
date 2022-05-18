8faa4cd3aa36d645c209821783bdfbf965758ed6
raphpay <41484808+raphpay@users.noreply.github.com>
2022-05-18 21:00:37 +0200

    Feature: Create i18n enum #6

---
+++ MyPass_W/Utils/i18n-Enum.swift
@@ -0,0 +1,23 @@
+//
+//  i18n-Enum.swift
+//  MyPass_W
+//
+//  Created by Raphaël Payet on 18/05/2022.
+//
+
+import Foundation
+
+enum i18n {
+    case generate, copy, textfield
+    
+    var translation: String {
+        switch self {
+        case .generate:
+            return "i18n_generate"
+        case .copy:
+            return "i18n_copy"
+        case .textfield:
+            return "i18n_textField"
+        }
+    }
+}
