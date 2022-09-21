92fff2e33384c1888c338aca4232d05cf6455481
raphpay <41484808+raphpay@users.noreply.github.com>
2022-09-21 21:45:48 +0200

    Feature: Implement donation screen UI #12

---
+++ MyPass_W/View/Components/Molecules/DonationButton.swift
@@ -0,0 +1,20 @@
+//
+//  DonationButton.swift
+//  MyPass_W
+//
+//  Created by Raphaël Payet on 21/09/2022.
+//
+
+import SwiftUI
+
+struct DonationButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DonationButton_Previews: PreviewProvider {
+    static var previews: some View {
+        DonationButton()
+    }
+}
